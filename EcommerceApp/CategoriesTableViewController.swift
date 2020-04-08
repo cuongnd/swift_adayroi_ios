85f2881f00cef0d86196c3bbaf54b74ba62af265
cuongnd <nguyendinhcuong@gmail.com>
2020-04-09 00:50:08 +0700

    update

--- EcommerceApp/CategoriesTableViewController.swift
+++ EcommerceApp/CategoriesTableViewController.swift
@@ -48,13 +48,13 @@ class CategoriesTableViewController: UITableViewController {
             let index = IndexPath.init(item: counter, section: 0)
             print("index")
             print(index)
-            self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            //self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
             pageView.currentPage = counter
             counter += 1
         } else {
             counter = 0
-            let index = IndexPath.init(item: counter, section: 0)
-            self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
+            //let index = IndexPath.init(item: counter, section: 0)
+            //self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
             pageView.currentPage = counter
             counter = 1
         }
@@ -90,38 +90,5 @@ class CategoriesTableViewController: UITableViewController {
             productsVC.title = category.name
             self.navigationController?.pushViewController(productsVC, animated: true)
         }
-    }
-}
-extension CategoriesTableViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imgArr.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        if let vc = cell.viewWithTag(111) as? UIImageView {
-            vc.image = imgArr[indexPath.row]
-        }
-        return cell
-    }
-}
-
-extension CategoriesTableViewController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = sliderCollectionView.frame.size
-        return CGSize(width: size.width, height: size.height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0.0
     }
 }
