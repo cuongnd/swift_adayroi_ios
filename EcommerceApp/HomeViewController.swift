421e5495960926868044bd71a337b0ecf4ab6f39
cuongnd <nguyendinhcuong@gmail.com>
2020-04-10 06:28:29 +0700

    update

--- EcommerceApp/HomeViewController.swift
+++ EcommerceApp/HomeViewController.swift
@@ -23,15 +23,51 @@ class HomeViewController: UIViewController ,UITableViewDataSource,UIScrollViewDe
         "Dakota Johnson"
     ]
     
+    var imgArr = [  UIImage(named:"Alexandra Daddario"),
+                    UIImage(named:"Angelina Jolie") ,
+                    UIImage(named:"Anne Hathaway") ,
+                    UIImage(named:"Dakota Johnson") ,
+                    UIImage(named:"Emma Stone") ,
+                    UIImage(named:"Emma Watson") ,
+                    UIImage(named:"Halle Berry") ,
+                    UIImage(named:"Jennifer Lawrence") ,
+                    UIImage(named:"Jessica Alba") ,
+                    UIImage(named:"Scarlett Johansson") ]
     
+    var timer = Timer()
+    var counter = 0
+    @IBOutlet weak var pageView: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource=self
         topSlideshowCollectionView.dataSource=self
 
+        
+        pageView.numberOfPages = imgArr.count
+        pageView.currentPage = 0
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    @objc func changeImage() {
+        
+        if counter < imgArr.count {
+            let index = IndexPath.init(item: counter, section: 0)
+            self.topSlideshowCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+            pageView.currentPage = counter
+            counter += 1
+        } else {
+            counter = 0
+            let index = IndexPath.init(item: counter, section: 0)
+            self.topSlideshowCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
+            pageView.currentPage = counter
+            counter = 1
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -75,16 +111,41 @@ class HomeViewController: UIViewController ,UITableViewDataSource,UIScrollViewDe
 extension HomeViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        if(collectionView.tag==0){
+            return imgArr.count;
+        }else if(collectionView.tag==1){
+            return photos.count;
+        }
+        return photos.count;
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "top_slideshow", for: indexPath);
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell_collection", for: indexPath);
         
         
         return cell
     }
     
     
+}
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = topSlideshowCollectionView.frame.size
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
 }
 
