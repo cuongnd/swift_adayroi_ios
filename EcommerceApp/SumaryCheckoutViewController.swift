b0912e5c311fe2d57f65aa94c9499d82aeeca4d2
cuongnd <nguyendinhcuong@gmail.com>
2020-04-26 23:42:55 +0700

    update source code

--- EcommerceApp/SumaryCheckoutViewController.swift
+++ EcommerceApp/SumaryCheckoutViewController.swift
@@ -22,6 +22,86 @@ class SumaryCheckoutViewController: UIViewController {
             self.view.setNeedsLayout()
         }
     }
+    @IBOutlet weak var UILabelTotalCostPayout: UILabel!
+    @IBOutlet weak var UILabelTaxShipingCost: UILabel!
+    @IBOutlet weak var UILabelShipingCost: UILabel!
+    @IBOutlet weak var UILabelTaxCost: UILabel!
+    @IBOutlet weak var UILabelTotalCostAffterCouponCode1: UILabel!
+    @IBOutlet weak var UILabelTotalCostAfterCouponCode: UILabel!
+    @IBOutlet weak var UILabelCouponCost: UILabel!
+    var cartManager: ShoppingCartManager?;
+     var activityIndicator:UIActivityIndicatorView = UIActivityIndicatorView()
+    
+   
+    @IBOutlet weak var UILabelTotalProduct: UILabel!
+    
+    
+    @IBOutlet weak var UILabelTotalCostBeforeTax: UILabel!
+    @IBAction func UIButtonCheckCoupon(_ sender: UIButton) {
+        return;
+        let coupon_code=UITextFieldCouponCode.text;
+        
+        if(coupon_code!.trimmingCharacters(in: .whitespacesAndNewlines)==""){
+            UITextFieldCouponCode.text=""
+            let alert = UIAlertController(title: "Thông báo", message: "Vui lòng nhập mã coupon", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Đã hiểu", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+        let url = AppConfiguration.root_url+"api/products/"+product_id
+        print("url")
+        print(url)
+        let request = NSMutableURLRequest(url: URL(string: url)!)
+        
+        let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            if (error != nil) {
+                print(error!.localizedDescription) // On indique dans la console ou est le problème dans la requête
+            } else {
+                if let content = data {
+                    DispatchQueue.main.async {
+                        do {
+                            //array
+                            let json_product = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String: AnyObject]
+                            
+                            let alert = UIAlertController(title: "Thông báo", message: "Mã coupon không hơp lệ", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                            self.present(alert, animated: true)
+                            
+                            self.activityIndicator.stopAnimating()
+                            UIApplication.shared.beginIgnoringInteractionEvents()
+                            
+                            
+                            
+                            
+                            
+                        } catch {
+                            
+                        }
+                    }
+                }
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode devrait être de 200, mais il est de \(httpStatus.statusCode)")
+                print("réponse = \(response)") // On affiche dans la console si le serveur ne nous renvoit pas un code de 200 qui est le code normal
+            }
+            
+            
+            if error == nil {
+                // Ce que vous voulez faire.
+            }
+        }
+        requestAPI.resume()
+        
+        
+        
+        
+        
+    }
+    @IBOutlet weak var UITextFieldCouponCode: UITextField!
     var jsonAddressShippingAndBinding: [String:String] = [:]
      var bodyContentHeight:CGFloat = 0.0
     var heightConstraint: NSLayoutConstraint!
@@ -43,6 +123,13 @@ class SumaryCheckoutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //updateContentViewHeight()
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        UILabelTotalProduct.text=String(describing: cartManager?.distinctProductItems())
+        UILabelTotalCostBeforeTax.text=String(describing:cartManager?.totalPrice())+" VNĐ"
+        
     }
    
   
