b7fc543c63abfcf7c066f10e82097232f0bd9fef
cuongnd <nguyendinhcuong@gmail.com>
2020-04-26 22:01:44 +0700

    update source code

--- EcommerceApp/Utilities/LibraryUtilitiesUtility.swift
+++ EcommerceApp/Utilities/LibraryUtilitiesUtility.swift
@@ -13,4 +13,10 @@ class LibraryUtilitiesUtility {
         let view=ecommerceStoryboard.instantiateViewController(withIdentifier: "ProductDetailsViewController") as! ProductDetailsViewController
         navigationController.pushViewController(view, animated: true)
     }
+    static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
 }
