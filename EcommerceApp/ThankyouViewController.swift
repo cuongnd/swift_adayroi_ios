299637f58e9db6f9a850fc6fb9f57af600146ab9
cuongnd <nguyendinhcuong@gmail.com>
2020-04-26 19:38:27 +0700

    update source code

--- EcommerceApp/ThankyouViewController.swift
+++ EcommerceApp/ThankyouViewController.swift
@@ -19,9 +19,56 @@ class ThankyouViewController: UIViewController {
     
     
     
-        //self.rest_api_get_payment()
+        self.rest_api_get_order()
     }
-    
+    @IBOutlet weak var UILabelTotalProduct: UILabel!
+    @IBOutlet weak var UILabelOrderNumber: UILabel!
+    @IBOutlet weak var UICollectionViewListOrderProducts: UICollectionView!
+    func rest_api_get_order() {
+        let url = AppConfiguration.root_url+"api/payments/"
+        let request = NSMutableURLRequest(url: URL(string: url)!)
+        print("now start load categories data")
+        let requestAPI = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            if (error != nil) {
+                print(error!.localizedDescription) // On indique dans la console ou est le problème dans la requête
+            } else {
+                if let content = data {
+                    DispatchQueue.main.async {
+                        do {
+                            //array
+                            let payment_json = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                            self.list_payment=[Payment]()
+                            for current_payment in payment_json as! [[String: AnyObject]] {
+                                var payment_item: Payment
+                                payment_item = Payment(id: current_payment["_id"] as! String,name: current_payment["name"] as! String, imageUrl: current_payment["full_image_path"] as! String,payment_type:current_payment["payment_type"] as! String)
+                                self.list_payment.append(payment_item);
+                                
+                            }
+                            print("response payment")
+                            print(self.list_payment)
+                            self.UICollectionViewListOrderProducts.reloadData()
+                            self.activityIndicator.stopAnimating()
+                            UIApplication.shared.endIgnoringInteractionEvents()
+                        } catch {
+                            print("load error slideshow")
+                        }
+                    }
+                }
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode devrait être de 200, mais il est de \(httpStatus.statusCode)")
+                print("réponse = \(response)") // On affiche dans la console si le serveur ne nous renvoit pas un code de 200 qui est le code normal
+            }
+            
+            
+            if error == nil {
+                // Ce que vous voulez faire.
+            }
+        }
+        requestAPI.resume()
+        
+        
+    }
     
    
     
