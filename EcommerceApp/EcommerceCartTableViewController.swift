5428cff066ddd0a9e656d87c3ff9a5b0fab35df3
cuongnd <nguyendinhcuong@gmail.com>
2020-04-28 15:06:56 +0700

    update source code

--- EcommerceApp/EcommerceCartTableViewController.swift
+++ EcommerceApp/EcommerceCartTableViewController.swift
@@ -17,29 +17,22 @@ class CartTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         print("aaaaa")
-        print(cartManager?.distinctProductCount())
         tableView.tableFooterView = UIView()
          self.tableView.reloadData()
     }
     @objc
     fileprivate func didSendAddToCartNotification(notification: Notification) {
-        print("hrllo 3333")
         let product = notification.userInfo?["product"] as? Product;
         self.addProduct(product: product!)
        
         
     }
     func addProduct(product: Product) {
-        print("product_id")
         self.cartManager?.addProduct(product: product)
-        print("hello counter addProduct")
-        print(cartManager?.distinctProductCount())
        
     }
     @objc
     fileprivate func didClearCartNotification(notification: Notification) {
-        print("hello didClearCartNotification 123")
     }
     func didPlaceOrder() {
         let addressCheckoutViewControllerVC = StoryboardEntityProvider().AddressCheckoutViewControllerVC()
@@ -59,31 +52,23 @@ class CartTableViewController: UITableViewController {
         guard let cartManager = cartManager else {
             return 0
         }
-        print("hello counter tableView")
-        print(cartManager.distinctProductCount() + 2)
         return cartManager.distinctProductCount() + 2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("couter")
-        print(cartManager?.distinctProductCount())
         guard let cartManager = cartManager else {
-            print("helllo 2")
             return UITableViewCell()
         }
         if (indexPath.row < cartManager.distinctProductCount()) {
             let cell = tableView.dequeueReusableCell(withIdentifier: "CartTableViewCell", for: indexPath) as! CartTableViewCell
-            print("helllo 3")
             cell.configureCell(item: cartManager.distinctProductItems()[indexPath.row])
             
             return cell
         } else if (indexPath.row == cartManager.distinctProductCount()) {
-            print("helllo 4")
             let cell = tableView.dequeueReusableCell(withIdentifier: "CartTotalTableViewCell", for: indexPath) as! CartTotalTableViewCell
             cell.configureCell(total: cartManager.totalPrice())
             return cell
         }
-        print("helllo 5")
         let cell = tableView.dequeueReusableCell(withIdentifier: "CartPlaceOrderTableViewCell", for: indexPath) as! CartPlaceOrderTableViewCell
         cell.configureCell(cartManager: cartManager)
         cell.delegate = self
@@ -92,8 +77,10 @@ class CartTableViewController: UITableViewController {
     
     @objc
     fileprivate func didUpdateCart(notification: Notification) {
-        print("hello didUpdateCart")
         tableView.reloadData()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.rowHeight=UITableViewAutomaticDimension
     }
 }
 
