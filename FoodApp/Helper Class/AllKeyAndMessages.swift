92a246d94932b59a827f2ed50b2420af3f60a3f6
macosx <nguyendinhcuong@gmail.com>
2020-11-15 14:46:03 +0700

    update root api

--- FoodApp/Helper Class/AllKeyAndMessages.swift
+++ FoodApp/Helper Class/AllKeyAndMessages.swift
@@ -9,7 +9,7 @@ let ORENGE_COLOR = Color_RGBA(254, 114, 76, 1)
 let BLCK_COLOR = Color_RGBA(31, 33, 36, 1)
 let GRAY_COLOR = Color_RGBA(154, 154, 154, 1)
 
-let API_URL = "http://localhost/foody/api/"
+let API_URL = "http://api.dayroi.online"
 let Privacy_URL = "http://localhost/foody/privacy-policy"
 let UD_userId = "UD_userId"
 let UD_currency = "UD_currency"
