fff853e8b745a48420432856164f509f573d035f
macosx <nguyendinhcuong@gmail.com>
2020-11-19 08:57:20 +0700

    update cell

---
+++ FoodApp/Layouts/Cells/Product/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  FoodApp
+//
+//  Created by MAC OSX on 11/19/20.
+//  Copyright © 2020 Mitesh's MAC. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
