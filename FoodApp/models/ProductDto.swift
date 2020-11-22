bf75642b7f34f7ae72da2c379d398e7fc0ad4173
macosx <nguyendinhcuong@gmail.com>
2020-11-22 19:55:58 +0700

    update code

---
+++ FoodApp/models/ProductDto.swift
@@ -0,0 +1,16 @@
+//
+//  ProductDto.swift
+//  FoodApp
+//
+//  Created by MAC OSX on 11/22/20.
+//  Copyright © 2020 Mitesh's MAC. All rights reserved.
+//
+
+import Foundation
+class ProductDto {
+    var id: Int?
+    var name: String?
+    var desc: String?
+    
+    required init?() {}
+}
