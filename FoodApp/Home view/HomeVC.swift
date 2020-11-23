ade122da950a181cc8c078a70656cf0f4aac0201
macosx <nguyendinhcuong@gmail.com>
2020-11-23 15:54:23 +0700

    updte code

--- FoodApp/Home view/HomeVC.swift
+++ FoodApp/Home view/HomeVC.swift
@@ -284,7 +284,7 @@ extension HomeVC: UICollectionViewDelegate,UICollectionViewDataSource,UICollecti
         }else if collectionView == self.Collectioview_HomeCategoryList{
            return CGSize(width:(UIScreen.main.bounds.width) / 4, height: 120)
         }else if collectionView == self.Collectioview_HomeFeatureProductList{
-          return CGSize(width:(UIScreen.main.bounds.width) / 1, height: 170)
+          return CGSize(width:(UIScreen.main.bounds.width) / 1, height: 140)
         }else{
            return CGSize(width:(UIScreen.main.bounds.width) / 2, height: 120)
         }
