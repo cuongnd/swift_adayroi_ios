580233510d485a141ac68fafd7f160899f9ee77e
macosx <nguyendinhcuong@gmail.com>
2020-11-27 17:45:35 +0700

    update code

--- FoodApp/Components/test2/test2.swift
+++ FoodApp/Components/test2/test2.swift
@@ -1,17 +1,15 @@
 //
-//  HomeTitle.swift
+//  test2.swift
 //  FoodApp
 //
-//  Created by MAC OSX on 11/22/20.
+//  Created by MAC OSX on 11/26/20.
 //  Copyright © 2020 Mitesh's MAC. All rights reserved.
 //
 
 import UIKit
 @IBDesignable
-class HomeTitle: UIView {
+class test2: UIView {
 
-    @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var goToDetail: UILabel!
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -19,11 +17,6 @@ class HomeTitle: UIView {
         // Drawing code
     }
     */
-    @IBInspectable public var titleText: String = "" {
-        didSet {
-            self.title.text = titleText
-        }
-    }
     override init(frame: CGRect) {
         super.init(frame:frame)
         commitInit();
@@ -33,12 +26,10 @@ class HomeTitle: UIView {
         commitInit()
     }
     func commitInit(){
-        let bundle = Bundle(for: HomeTitle.self)
+        let bundle = Bundle(for: test2.self)
         //let viewHomeTitle=bundle.loadNibNamed("HomeTitle", owner: self, options: nil)
         
-        let viewHomeTitle=bundle.loadNibNamed("HomeTitle", owner: self, options: nil)![0] as! UIView
-        addSubview(viewHomeTitle)
+        let test2=bundle.loadNibNamed("test2", owner: self, options: nil)![0] as! UIView
+        addSubview(test2)
     }
-    
-
 }
