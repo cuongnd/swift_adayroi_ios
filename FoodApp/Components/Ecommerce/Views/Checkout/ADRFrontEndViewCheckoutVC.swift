a73728fd271bcef7e8b553be627382c9dbad0fbc
macosx <nguyendinhcuong@gmail.com>
2020-12-09 07:39:29 +0700

    update code

---
+++ FoodApp/Components/Ecommerce/Views/Checkout/ADRFrontEndViewCheckoutVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddtoCartVC.swift
+//  FoodApp
+//
+//  Created by Mitesh's MAC on 04/06/20.
+//  Copyright © 2020 Mitesh's MAC. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import SQLite
+import ETBinding
+import SlideMenuControllerSwift
+
+
+class ADRFrontEndViewCheckoutVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+    
+    
+    
+}
