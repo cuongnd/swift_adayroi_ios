3bbfcf6ba35a651e5703d922dd25e5db139c3a91
macosx <nguyendinhcuong@gmail.com>
2020-12-09 15:57:25 +0700

    update code

--- FoodApp/Components/Ecommerce/Views/Checkout/ADRFrontEndViewCheckoutVC.swift
+++ FoodApp/Components/Ecommerce/Views/Checkout/ADRFrontEndViewCheckoutVC.swift
@@ -15,6 +15,25 @@ import SlideMenuControllerSwift
 
 class ADRFrontEndViewCheckoutVC: UIViewController {
     
+    @IBOutlet weak var UITextFieldShippingFullName: UITextField!
+    @IBOutlet weak var UITextFieldShippingEmail: UITextField!
+    @IBOutlet weak var UITextFieldShippingPhonenumber: UITextField!
+    @IBOutlet weak var UITextViewShippingAddress1: UITextView!
+    @IBOutlet weak var UITextViewShippingAddress2: UITextView!
+    @IBOutlet weak var UISwitchSameShipping: UISwitch!
+    
+    
+    @IBOutlet weak var UITextFieldPaymentFullName: UITextField!
+    @IBOutlet weak var UITextFieldPaymentEmail: UITextField!
+    @IBOutlet weak var UITextFieldPaymentPhoneNumber: UITextField!
+    @IBOutlet weak var UITextViewPaymentAddress1: UITextView!
+    @IBOutlet weak var UITextViewPaymentAddress2: UITextView!
+    @IBOutlet weak var UITextViewNote: UITextView!
+    @IBOutlet weak var UIButtonNext: UIButton!
+    @IBOutlet weak var UIButtonBack: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
