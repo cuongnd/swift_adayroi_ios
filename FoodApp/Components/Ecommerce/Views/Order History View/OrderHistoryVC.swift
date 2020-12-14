41b5428e4c97fbdd4583cb52b905239c47073b67
macosx <nguyendinhcuong@gmail.com>
2020-12-14 19:19:35 +0700

    update code

--- FoodApp/Components/Ecommerce/Views/Order History View/OrderHistoryVC.swift
+++ FoodApp/Components/Ecommerce/Views/Order History View/OrderHistoryVC.swift
@@ -21,70 +21,8 @@ class OrderHistoryCell: UITableViewCell {
     @IBOutlet weak var lbl_itemPrice: UILabel!
     @IBOutlet weak var lbl_itemAddress: UILabel!
     @IBOutlet weak var lbl_itemQty: UILabel!
-    @IBOutlet weak var btn_Close: UIButton!
 }
-class OrderHistoryPickupCell: UITableViewCell {
-    
-    @IBOutlet weak var lbl_OrderNoLabel: UILabel!
-    @IBOutlet weak var lbl_QtyLabel: UILabel!
-    @IBOutlet weak var lbl_orderStatusLabel: UILabel!
-    
-    @IBOutlet weak var lbl_Date: UILabel!
-    @IBOutlet weak var lbl_PaymentType: UILabel!
-    @IBOutlet weak var lbl_OrderNumber: UILabel!
-    @IBOutlet weak var lbl_itemPrice: UILabel!
-    @IBOutlet weak var lbl_itemQty: UILabel!
-    
-    @IBOutlet weak var img_orderplaced: UIImageView!
-    @IBOutlet weak var lbl_1: UILabel!
-    @IBOutlet weak var lbl_orderplaced: UILabel!
-    
-    
-    @IBOutlet weak var img_orderready: UIImageView!
-    @IBOutlet weak var lbl_2: UILabel!
-    @IBOutlet weak var lbl_orderready: UILabel!
-    
-    @IBOutlet weak var img_orderdelivered: UIImageView!
-    @IBOutlet weak var lbl_orderdelivered: UILabel!
-    
-    
-    @IBOutlet weak var btn_Close: UIButton!
-    
-    
-}
-class OrderHistoryDeliverCell: UITableViewCell {
-    
-    @IBOutlet weak var lbl_OrderNoLabel: UILabel!
-    @IBOutlet weak var lbl_QtyLabel: UILabel!
-    @IBOutlet weak var lbl_orderStatusLabel: UILabel!
-    
-    @IBOutlet weak var lbl_Date: UILabel!
-    @IBOutlet weak var lbl_PaymentType: UILabel!
-    @IBOutlet weak var lbl_OrderNumber: UILabel!
-    @IBOutlet weak var lbl_itemPrice: UILabel!
-    
-    @IBOutlet weak var lbl_itemQty: UILabel!
-    
-    
-    @IBOutlet weak var img_orderplaced: UIImageView!
-    @IBOutlet weak var lbl_1: UILabel!
-    @IBOutlet weak var lbl_orderplaced: UILabel!
-    
-    
-    @IBOutlet weak var img_orderready: UIImageView!
-    @IBOutlet weak var lbl_2: UILabel!
-    @IBOutlet weak var lbl_orderready: UILabel!
-    
-    @IBOutlet weak var img_orderontheway: UIImageView!
-    @IBOutlet weak var lbl_3: UILabel!
-    @IBOutlet weak var lbl_orderontheway: UILabel!
-    
-    
-    @IBOutlet weak var img_orderdelivered: UIImageView!
-    @IBOutlet weak var lbl_orderdelivered: UILabel!
-    
-    @IBOutlet weak var btn_Close: UIButton!
-}
+
 
 class OrderHistoryVC: UIViewController {
     @IBOutlet weak var Tableview_OrderHistory: UITableView!
@@ -145,286 +83,27 @@ extension OrderHistoryVC: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let data = self.OrderHistoryData[indexPath.row]
-        if selected == "\(indexPath.row)"
-        {
-            if data["order_type"].stringValue == "1"
-            {
-                return 300
-            }
-            else{
-                return 245
-            }
-        }
-        else{
-            return 135
-        }
+        return 135
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let data = self.OrderHistoryData[indexPath.row]
-        if selected == "\(indexPath.row)"
-        {
-            if data["order_type"].stringValue == "1"
-            {
-                let cell = self.Tableview_OrderHistory.dequeueReusableCell(withIdentifier: "OrderHistoryDeliverCell") as! OrderHistoryDeliverCell
-                cell.lbl_QtyLabel.text = "QTY :".localiz()
-                cell.lbl_OrderNoLabel.text = "ORDER ID :".localiz()
-                cell.lbl_orderStatusLabel.text = "STATUS :".localiz()
-                cell.lbl_itemQty.text = data["qty"].stringValue
-                cell.lbl_Date.text = data["date"].stringValue
-                let setdate = DateFormater.getBirthDateStringFromDateString(givenDate:data["date"].stringValue)
-                cell.lbl_Date.text = setdate
-                let status = data["status"].stringValue
-                if status == "1"
-                {
-                    cell.lbl_orderplaced.text = "Order placed"
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = GRAY_COLOR
-                    cell.lbl_orderontheway.textColor = GRAY_COLOR
-                    cell.lbl_orderdelivered.textColor = GRAY_COLOR
-                    
-                    
-                    cell.lbl_1.backgroundColor = GRAY_COLOR
-                    cell.lbl_2.backgroundColor = GRAY_COLOR
-                    cell.lbl_3.backgroundColor = GRAY_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "circle")
-                    cell.img_orderready.tintColor = GRAY_COLOR
-                    
-                    cell.img_orderontheway.image = UIImage(systemName: "circle")
-                    cell.img_orderontheway.tintColor = GRAY_COLOR
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "circle")
-                    cell.img_orderdelivered.tintColor = GRAY_COLOR
-                    
-                    
-                }
-                else if status == "2"
-                {
-                    cell.lbl_orderready.text = "Order ready"
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = ORENGE_COLOR
-                    cell.lbl_orderontheway.textColor = GRAY_COLOR
-                    cell.lbl_orderdelivered.textColor = GRAY_COLOR
-                    
-                    cell.lbl_1.backgroundColor = ORENGE_COLOR
-                    cell.lbl_2.backgroundColor = GRAY_COLOR
-                    cell.lbl_3.backgroundColor = GRAY_COLOR
-                    
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderplaced.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderready.tintColor = ORENGE_COLOR
-                    
-                    
-                    cell.img_orderontheway.image = UIImage(systemName: "circle")
-                    cell.img_orderontheway.tintColor = GRAY_COLOR
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "circle")
-                    cell.img_orderdelivered.tintColor = GRAY_COLOR
-                    
-                    
-                }
-                else if status == "3"
-                {
-                    cell.lbl_orderontheway.text = "Order on the way"
-                    
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = ORENGE_COLOR
-                    cell.lbl_orderontheway.textColor = ORENGE_COLOR
-                    cell.lbl_orderdelivered.textColor = GRAY_COLOR
-                    
-                    cell.lbl_1.backgroundColor = ORENGE_COLOR
-                    cell.lbl_2.backgroundColor = ORENGE_COLOR
-                    cell.lbl_3.backgroundColor = GRAY_COLOR
-                    
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderplaced.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderready.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderontheway.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderontheway.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "circle")
-                    cell.img_orderdelivered.tintColor = GRAY_COLOR
-                    
-                }
-                else if status == "4"
-                {
-                    
-                    cell.lbl_orderdelivered.text = "Order delivered"
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = ORENGE_COLOR
-                    cell.lbl_orderontheway.textColor = ORENGE_COLOR
-                    cell.lbl_orderdelivered.textColor = ORENGE_COLOR
-                    
-                    
-                    cell.lbl_1.backgroundColor = ORENGE_COLOR
-                    cell.lbl_2.backgroundColor = ORENGE_COLOR
-                    cell.lbl_3.backgroundColor = ORENGE_COLOR
-                    
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderplaced.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderready.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderontheway.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderontheway.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderdelivered.tintColor = ORENGE_COLOR
-                }
-                cell.lbl_OrderNumber.text = data["order_number"].stringValue
-                let ItemPrice = formatter.string(for: data["total_price"].stringValue.toDouble)
-                cell.lbl_itemPrice.text = "\(UserDefaultManager.getStringFromUserDefaults(key: UD_currency))\(ItemPrice!)"
-                let paymentType = data["payment_type"].stringValue
-                if paymentType == "0"
-                {
-                    cell.lbl_PaymentType.text = "PAY BY CASH".localiz()
-                }
-                else if paymentType == "1"
-                {
-                    cell.lbl_PaymentType.text = "RAZORPAY".localiz()
-                }
-                cell.btn_Close.tag = indexPath.row
-                cell.btn_Close.addTarget(self, action: #selector(btnTapclose), for: .touchUpInside)
-                return cell
-            }
-            else if data["order_type"].stringValue == "2"{
-                let cell = self.Tableview_OrderHistory.dequeueReusableCell(withIdentifier: "OrderHistoryPickupCell") as! OrderHistoryPickupCell
-                cell.lbl_QtyLabel.text = "QTY :".localiz()
-                cell.lbl_OrderNoLabel.text = "ORDER ID :".localiz()
-                cell.lbl_orderStatusLabel.text = "STATUS :".localiz()
-                cell.lbl_itemQty.text = data["qty"].stringValue
-                cell.lbl_Date.text = data["date"].stringValue
-                let setdate = DateFormater.getBirthDateStringFromDateString(givenDate:data["date"].stringValue)
-                cell.lbl_Date.text = setdate
-                let status = data["status"].stringValue
-                if status == "1"
-                {
-                    cell.lbl_orderplaced.text = "Order placed"
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = GRAY_COLOR
-                    cell.lbl_orderdelivered.textColor = GRAY_COLOR
-                    
-                    
-                    cell.lbl_1.backgroundColor = GRAY_COLOR
-                    cell.lbl_2.backgroundColor = GRAY_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "circle")
-                    cell.img_orderready.tintColor = GRAY_COLOR
-                    
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "circle")
-                    cell.img_orderdelivered.tintColor = GRAY_COLOR
-                    
-                    
-                }
-                else if status == "2"
-                {
-                    cell.lbl_orderready.text = "Order ready"
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = ORENGE_COLOR
-                    cell.lbl_orderdelivered.textColor = GRAY_COLOR
-                    
-                    cell.lbl_1.backgroundColor = ORENGE_COLOR
-                    cell.lbl_2.backgroundColor = GRAY_COLOR
-                    
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderplaced.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderready.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "circle")
-                    cell.img_orderdelivered.tintColor = GRAY_COLOR
-                    
-                    
-                    
-                }
-                else if status == "4"
-                {
-                    
-                    cell.lbl_orderdelivered.text = "Order delivered"
-                    cell.lbl_orderplaced.textColor = ORENGE_COLOR
-                    cell.lbl_orderready.textColor = ORENGE_COLOR
-                    cell.lbl_orderdelivered.textColor = ORENGE_COLOR
-                    
-                    
-                    cell.lbl_1.backgroundColor = ORENGE_COLOR
-                    cell.lbl_2.backgroundColor = ORENGE_COLOR
-                    
-                    cell.img_orderplaced.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderplaced.tintColor = ORENGE_COLOR
-                    
-                    cell.img_orderready.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderready.tintColor = ORENGE_COLOR
-                    
-                    
-                    cell.img_orderdelivered.image = UIImage(systemName: "checkmark.circle.fill")
-                    cell.img_orderdelivered.tintColor = ORENGE_COLOR
-                }
-                cell.lbl_OrderNumber.text = data["order_number"].stringValue
-                let ItemPrice = formatter.string(for: data["total_price"].stringValue.toDouble)
-                cell.lbl_itemPrice.text = "\(UserDefaultManager.getStringFromUserDefaults(key: UD_currency))\(ItemPrice!)"
-                let paymentType = data["payment_type"].stringValue
-                if paymentType == "0"
-                {
-                    cell.lbl_PaymentType.text = "PAY BY CASH".localiz()
-                }
-                else if paymentType == "1"
-                {
-                    cell.lbl_PaymentType.text = "RAZORPAY".localiz()
-                }
-                cell.btn_Close.tag = indexPath.row
-                cell.btn_Close.addTarget(self, action: #selector(btnTapclose), for: .touchUpInside)
-                return cell
-            }
-            
-        }
-        else{
-            let cell = self.Tableview_OrderHistory.dequeueReusableCell(withIdentifier: "OrderHistoryCell") as! OrderHistoryCell
-            cell.lbl_QtyLabel.text = "QTY :".localiz()
-            cell.lbl_OrderNoLabel.text = "ORDER ID :".localiz()
-            cell.lbl_orderStatusLabel.text = "STATUS :".localiz()
-            cell.lbl_itemQty.text = data["qty"].stringValue
-            cell.lbl_Date.text = data["date"].stringValue
-            //let setdate = DateFormater.getBirthDateStringFromDateString(givenDate:data["created_date"].stringValue)
-            //cell.lbl_Date.text = setdate
-            let status = data["order_status_id"].stringValue
-            if status == "1"
-            {
-                cell.lbl_itemAddress.text = "Order placed"
-            }
-            else if status == "2"
-            {
-                cell.lbl_itemAddress.text = "Order ready"
-            }
-            else if status == "3"
-            {
-                cell.lbl_itemAddress.text = "Order on the way"
-            }
-            else if status == "4"
-            {
-                cell.lbl_itemAddress.text = "Order delivered"
-            }
-            cell.lbl_OrderNumber.text = data["order_number"].stringValue
-            let ItemPrice = formatter.string(for: data["total"].stringValue.toDouble)
-            cell.lbl_itemPrice.text = "\(UserDefaultManager.getStringFromUserDefaults(key: UD_currency))\(ItemPrice!)"
-            cell.lbl_PaymentType.text =  data["payment_method_name"].stringValue
-            cell.btn_Close.tag = indexPath.row
-            cell.btn_Close.addTarget(self, action: #selector(btnTapopen), for: .touchUpInside)
-            return cell
-        }
-        return UITableViewCell()
+        
+       let cell = self.Tableview_OrderHistory.dequeueReusableCell(withIdentifier: "OrderHistoryCell") as! OrderHistoryCell
+        var data = self.OrderHistoryData[indexPath.row]
+        cell.lbl_QtyLabel.text = "QTY :".localiz()
+        cell.lbl_OrderNoLabel.text = "ORDER ID :".localiz()
+        cell.lbl_orderStatusLabel.text = "STATUS :".localiz()
+        cell.lbl_itemQty.text = data["qty"].stringValue
+        cell.lbl_Date.text = data["date"].stringValue
+        //let setdate = DateFormater.getBirthDateStringFromDateString(givenDate:data["created_date"].stringValue)
+        //cell.lbl_Date.text = setdate
+        let status = data["order_status_id"].stringValue
+        
+        cell.lbl_OrderNumber.text = data["order_number"].stringValue
+        let ItemPrice = formatter.string(for: data["total"].stringValue.toDouble)
+        cell.lbl_itemPrice.text = "\(UserDefaultManager.getStringFromUserDefaults(key: UD_currency))\(ItemPrice!)"
+        cell.lbl_PaymentType.text =  data["payment_method_name"].stringValue
+        return cell
         
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -434,16 +113,7 @@ extension OrderHistoryVC: UITableViewDelegate,UITableViewDataSource {
         vc.status = data["status"].stringValue
         self.navigationController?.pushViewController(vc, animated: true)
     }
-    @objc func btnTapopen(sender:UIButton)
-    {
-        self.selected = "\(sender.tag)"
-        self.Tableview_OrderHistory.reloadData()
-    }
-    @objc func btnTapclose(sender:UIButton)
-    {
-        self.selected = ""
-        self.Tableview_OrderHistory.reloadData()
-    }
+    
 }
 //MARK: Webservices
 extension OrderHistoryVC {
